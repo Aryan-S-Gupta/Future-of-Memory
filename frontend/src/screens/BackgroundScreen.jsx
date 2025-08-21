72b501af748cefd59dfca25f94b4fe9690acbc4f
dhruvanil <dhruv2605@gmail.com>
2025-08-21 21:04:45 +1000

    finished background screen

--- frontend/src/screens/BackgroundScreen.jsx
+++ frontend/src/screens/BackgroundScreen.jsx
@@ -12,16 +12,18 @@ const BackgroundScreen = () => {
   */
   return (
     <BasePage>
-      <h1 className="title2">Background</h1>
+      <h1 className="title">Background</h1>
       <div className="text-container2">
       <p className="text"> 
         The year is 2040.
-        Neurotechnology has advanced beyond imagination, allowing memories to be edited, emotions regulated, and even consciousness shared. These breakthroughs have brought progress, but also deep ethical uncertainty.
-        You are a senior decision-maker at the Global Neurotechnology Regulation Authority (GNRA).
-        Your responsibility: evaluate new applications of neurotech and decide whether they should be encouraged, restricted, or banned.
-        Every choice you make will ripple outward, shaping how societies evolve, how people live, and how humanity defines itself in this new age of the mind.
-        Your task is not simple. Progress, profit, freedom, and control are all at stake.
-        The future of human thought begins with your decisions.
+        Neurotechnology has advanced beyond imagination, allowing memories to be <br /> <br />
+        edited, emotions regulated, and even consciousness shared. These breakthroughs have brought <br /> <br />
+        progress, but also deep ethical uncertainty. You are a senior decision-maker at the Global <br /> <br />
+        Neurotechnology Regulation Authority (GNRA). Your responsibility: evaluate new applications of <br /> <br /> 
+        neurotech and decide whether they should be encouraged, restricted, or banned. Every choice <br /> <br /> 
+        you make will ripple outward, shaping how societies evolve, how people live, and how humanity <br /> <br />
+        defines itself in this new age of the mind. Your task is not simple. Progress, profit, freedom, and <br /> <br />
+        control are all at stake. The future of human thought begins with your decisions.
       </p>
       </div>
       <div className="button-group">
