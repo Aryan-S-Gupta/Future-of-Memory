a1baf8a42d70432272b230cade84eca6d01f50b3
manyakhosla <manyakhosla05@gmail.com>
2025-08-26 15:26:20 +1000

    fixed a very small typo

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('storyline/start', views.get_story_background, name='storyline_scenario'),
+    path('storyline/start', views.get_story_scenario, name='storyline_background'),
     path('storyline/question', views.get_story_question, name='storyline_question'),
     path('storyline/choice', views.get_story_result_by_choice, name='storyline_choice'),
     path('storyline/result', views.get_story_result, name='storyline_result')
