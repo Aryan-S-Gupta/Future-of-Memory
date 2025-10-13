87a21d012e5c0d07cae1915b11d200fc3d820679
manyakhosla <manyakhosla05@gmail.com>
2025-10-13 15:33:15 +1000

    imported styles all

--- frontend/src/screens/HostGame/HostBackgroundScreen.jsx
+++ frontend/src/screens/HostGame/HostBackgroundScreen.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import BasePage from "../BasePage.jsx";
 import Button from "../../components/Button/Button.jsx";
-import "../../styles/BackgroundScreen.css";
+import "../../styles/";
 import ExitExperience from "../../components/ExitExperience/ExitExperience.jsx";
 import { checkGameStarted } from "../../../api/multiplayer/RoomManagementApi.js";
 import { startGame } from "../../../api/multiplayer/RoomManagementApi.js";
